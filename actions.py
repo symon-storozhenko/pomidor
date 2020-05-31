de204a1f815b98feeae1bda57d2fc5a57b66da4b
Symon Storozhenko <stosimon@gmail.com>
2020-05-31 17:00:46 -0400

    #1 tomato3: created actions file with two classes

---
+++ actions.py
@@ -0,0 +1,21 @@
+class ForwardAction:
+    def __init__(self, action):
+        self.forward_action_dictionary = {
+            '*click': 'click()',
+            '*type': 'send_keys("Yoyo!!!!")'
+        }[action]
+
+
+class BackwardAction:
+    def __init__(self, action):
+        self.backward_actions_dictionary = {
+            '*visible': 'is_displayed()',
+            '*not visible': 'test'
+        }[action]
+
+
+vector = 'forward_action_dictionary'
+
+act = ForwardAction('*click')
+
+print(act.forward_action_dictionary)
---
+++ pageObjects/page_factory.py
@@ -0,0 +1,12 @@
+class PageObject:
+    def __init__(self, obj):
+        self.home_page = {
+            'login_field': ['XPATH', "//input[@name=\"username\"]"],
+            'password_field': ['XPATH', "//input[@name=\"password\"]"],
+            'submit_button': ['CSS', "//button[@type=\"submit\"]"],
+        }[obj]
+
+
+po = PageObject('login_field')
+if True:
+    print('hey!', po.home_page[1])
